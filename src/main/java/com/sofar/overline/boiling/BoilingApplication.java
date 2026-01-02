3decc7aa93e1e55f67df0420c8736ef103d3d1a4
richard <ambefr@gmail.com>
2026-01-02 15:24:15 +0100

    First commit

---
+++ src/main/java/com/sofar/overline/boiling/BoilingApplication.java
@@ -0,0 +1,13 @@
+package com.sofar.overline.boiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofar/overline/boiling/BoilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofar.overline.boiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
