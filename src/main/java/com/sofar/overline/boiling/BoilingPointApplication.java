b0fed78aef2c144a02a5aead101857a19086da4e
richard <ambefr@gmail.com>
2026-01-08 15:37:31 +0100

    Rename project

--- src/main/java/com/sofar/overline/boiling/BoilingPointApplication.java
+++ src/main/java/com/sofar/overline/boiling/BoilingPointApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoilpointApplication {
+public class BoilingPointApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(BoilpointApplication.class, args);
+        SpringApplication.run(BoilingPointApplication.class, args);
     }
 
 }
